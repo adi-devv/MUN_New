fb1b4d3294fc7da1602e43c4a77062de3680f23b
Aadit Singal <79838616+adi-devv@users.noreply.github.com>
2024-04-27 01:26:10 +0530

    Add files via upload

---
+++ Header/menuData.tsx
@@ -0,0 +1,9 @@
+const menuData = [
+  { title: "Home", path: "#home" },
+  { title: "About", path: "#features" },
+  { title: "Events", path: "#Events" },
+  { title: "Our Team", path: "#team" },
+  { title: "Contact", path: "#footer" },
+];
+
+export default menuData;
